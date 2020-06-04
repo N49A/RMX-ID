499bf0775ca17b9814241fd5320591eb6e4cc578
N49A <66381829+N49A@users.noreply.github.com>
2020-06-04 20:27:54 +0700

    Update DarkFB.py

--- DarkFB.py
+++ DarkFB.py
@@ -65,7 +65,44 @@ listgrup = []
 vulnot = '\x1b[31mNot Vuln'
 vuln = '\x1b[32mVuln'
 
+def siapa():
+	os.system('clear')
+	nama = raw_input("\033[1;97mSiapa nama kamu ? \033[1;91m: \033[1;92m")
+	if nama =="":
+		print"\033[1;96m[!] \033[1;91mIsi yang benar"
+		time.sleep(1)
+		siapa()
+	else:
+		os.system('clear')
+		jalan("\033[1;97mSelamat datang \033[1;92m" +nama+ "\n\033[1;97mTerimakasih telah menggunakan tools ini !!")
+		time.sleep(1)
+		loginSC()
+		
+		
+def loginSC():
+	os.system('clear')
+	print"\033[1;97mSilahkan login SC nya dulu bosque\n"
+	username = raw_input("\033[1;96m[*] \033[1;97mUsername \033[1;91m: \033[1;92m")
+	password = raw_input("\033[1;96m[*] \033[1;97mPassword \033[1;91m: \033[1;92m")
+	if username =="dark" and password =="fb":
+		print"\033[1;96m[✓] \033[1;92mLogin success"
+		time.sleep(1)
+		login()
+	else:
+		print"\033[1;96m[!] \033[1;91mSalah!!"
+		time.sleep(1)
+                LoginSC()
 
+def login():
+	os.system('clear')
+	try:
+		toket = open('login.txt','r')
+		menu() 
+	except (KeyError,IOError):
+		os.system('clear')
+		print logo
+		print 42*"\033[1;96m="
+		print('\033[1;96m[☆] \x1b[
 def login():
     os.system('clear')
     try:

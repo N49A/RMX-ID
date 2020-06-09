5ab070bc9a44bba59ac5df05b378c9c4f2b17326
N49A <66381829+N49A@users.noreply.github.com>
2020-06-10 01:10:23 +0700

    Update Dark.py

--- Dark.py
+++ Dark.py
@@ -105,7 +105,7 @@ while (loop == 'true'):
     	password = raw_input("\033[1;96m[☆] \x1b[1;93mPassword Tool \x1b[1;96m>>>> ")
         if (password == CorrectPassword):
             print "LOGIN SUCCESS" + username
-            loginSC()
+            loop = '
         else:
             print "password salah!"
             os.system('xdg-open https://http://wa.me/6281273996358')

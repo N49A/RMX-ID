626926e7a040cef78d3efe86dcf12186d332813c
N49A <66381829+N49A@users.noreply.github.com>
2020-06-10 13:19:41 +0700

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -80,7 +80,7 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print """
 \033[1;97m╔╗───╔═══╗╔═══╗╔══╗╔═╗─╔╗
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
+\033[1;97m║║───║╔═╗║║╔═╗║╚╣─╝║║╚╗║║
 \033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
 \033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
